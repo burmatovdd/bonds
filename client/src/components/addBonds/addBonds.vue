fe2160ccfed0f0a9fb335eda6220a620a94917b6
Andrew Dyachenko <andrey.dyachenko@zfx.com>
2022-04-19 12:57:48 +0300

    fix(.gitignore): extend with ./data folder

--- client/src/components/addBonds/addBonds.vue
+++ client/src/components/addBonds/addBonds.vue
@@ -1,42 +1,52 @@
 <template>
-<div class="container">
-  <div class="add">
-    <input id="inputBond" type="text" class="input" placeholder="enter bond">
-    <input id="inputBondCount" type="text" class="input" placeholder="enter count">
-    <button class="addBonds" @click="sendBond">Add</button>
-  </div>
-</div>
+    <div class="container">
+        <div class="add">
+            <input
+                id="inputBond"
+                type="text"
+                class="input"
+                placeholder="enter bond"
+            />
+            <input
+                id="inputBondCount"
+                type="text"
+                class="input"
+                placeholder="enter count"
+            />
+            <button class="addBonds" @click="sendBond">Add</button>
+        </div>
+    </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: "addBonds",
-  setup(){
-    let bonds = {
-      bond: null,
-      count: null,
-    }
-    async function sendBond() {
-      bonds.bond = document.getElementById("inputBond").value;
-      bonds.count = document.getElementById("inputBondCount").value;
+    name: "addBonds",
+    setup() {
+        let bonds = {
+            bond: null,
+            count: null,
+        };
+        async function sendBond() {
+            bonds.bond = document.getElementById("inputBond").value;
+            bonds.count = document.getElementById("inputBondCount").value;
 
-      let sendUrl = "http://localhost:8080/bonds";
+            let sendUrl = "http://localhost:8080/bonds";
 
-      await fetch(sendUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bonds)
-      })
-    }
-    return {
-      bonds,
-      sendBond
-    }
-  }
-})
+            await fetch(sendUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(bonds),
+            });
+        }
+        return {
+            bonds,
+            sendBond,
+        };
+    },
+});
 </script>
 
 <style>
